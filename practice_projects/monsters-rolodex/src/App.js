cb7654ab3fecd2100b3a7fd94ee6ee55888a5a4c
Xiao Fan <xiao.fan4@huawei.com>
2022-12-31 01:13:56 +0100

    12-30

--- practice_projects/monsters-rolodex/src/App.js
+++ practice_projects/monsters-rolodex/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField: "",
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((users) =>
+                this.setState(
+                    () => {
+                        return { monsters: users };
+                    },
+                    () => console.log(this.state.monsters)
+                )
+            );
+    }
+
+    render() {
+        console.log("render");
+        const filterMonsters = this.state.monsters.filter((monster) =>
+            monster.name.toLocaleLowerCase().includes(this.state.searchField)
+        );
+        return (
+            <div className="App">
+                <input
+                    className="search-box"
+                    type="search"
+                    placeholder="search monsters"
+                    onChange={(event) => {
+                        const searchField =
+                            event.target.value.toLocaleLowerCase();
+
+                        this.setState(() => {
+                            return { searchField }; // 这里只需要修改状态, 下面后自动渲染
+                        });
+                    }}
+                />
+                {filterMonsters.map((monster) => {
+                    return (
+                        <div key={monster.id}>
+                            <h2>{monster.name}</h2>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
